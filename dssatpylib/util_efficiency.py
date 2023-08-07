88bdccf4b70a7a0841dc21a5a23e68782abb8025
Agro-Climatic Tools <49209645+AgroClimaticTools@users.noreply.github.com>
2023-08-07 08:48:35 -0400

    Added more metrics

--- dssatpylib/util_efficiency.py
+++ dssatpylib/util_efficiency.py
@@ -6,7 +6,9 @@ Description: Calculates model efficiencies and other model validation metrics
 
 """
 import math
-from sklearn.metrics import r2_score
+import numpy as np
+from sklearn.metrics import r2_score, mean_squared_error
+from scipy import stats
 
 # Computes RMSE coefficient
 def RMSE(observed, simulated):
@@ -16,13 +18,34 @@ def RMSE(observed, simulated):
     :param  observed: numpy array or list of observed data
     :param simulated: numpy array or list of simulated data
     
-    :return float: RMSE in the simulated data
+    :return float: RMSE
     '''
-    sum = 0.
-    for i in range(len(observed)):
-            sum += ((observed[i]-simulated[i])**2)
-    sum = sum / len(observed)
-    return (math.sqrt(sum))
+    return mean_squared_error(observed, simulated, squared=False)
+
+# Computes RMSE coefficient
+def nRMSE(observed, simulated):
+    '''
+    Calculates the Relative Root Mean Squared Error (RMSE) value of the simulated data
+
+    :param  observed: numpy array or list of observed data
+    :param simulated: numpy array or list of simulated data
+    
+    :return float: RRMSE
+    '''
+    return mean_squared_error(observed, simulated, squared=False)/np.mean(observed)
+
+# Computes RSR coefficient
+def RSR(observed, simulated):
+    '''
+    Calculates the Root Mean Squared Error to Standard deviation Ratio (RSR)
+    value of the simulated data
+
+    :param  observed: numpy array or list of observed data
+    :param simulated: numpy array or list of simulated data
+    
+    :return float: RSR
+    '''
+    return mean_squared_error(observed, simulated, squared=False)/np.std(observed)
 
 # Computes Nash Sutcliffe Efficiency
 def NSE(observed, simulated):
@@ -32,16 +55,11 @@ def NSE(observed, simulated):
     :param  observed: numpy array or list of observed data
     :param simulated: numpy array or list of simulated data
     
-    :return float: NSE in the simulated data
+    :return float: NSE
     '''
-    sum_obs, diff_num, diff_den = 0.0, 0.0, 0.0
-    for record in observed:
-        sum_obs += record
-    mean_obs = sum_obs/len(observed)
-    for count in range(len(observed)):
-        diff_num += pow((observed[count]-simulated[count]), 2)
-        diff_den += pow((observed[count]-mean_obs), 2)
-    NSE = 1-(diff_num/diff_den)
+    num = np.sum((observed-simulated)**2)
+    den = np.sum((observed-np.mean(simulated))**2)
+    NSE = 1-num/den
     return (NSE)
 
 def R2(observed, simulated):
@@ -55,4 +73,15 @@ def R2(observed, simulated):
     :return float: R2 between observed and simulated data
     '''
     return r2_score(observed, simulated)
+
+def dStat(observed, simulated):
+    '''
+    Performs the (one-sample or two-sample) Kolmogorov-Smirnov (d-stat) test for
+    goodness of fit between observed and simulated data
     
+    :param  observed: numpy array or list of observed data
+    :param simulated: numpy array or list of simulated data
+    
+    :return float: d-Stat between observed and simulated data
+    '''
+    return stats.kstest(observed, simulated).pvalue
