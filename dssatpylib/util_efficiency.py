9592685abafe2184a4de2d8ae8ec145d9353529e
Agro-Climatic Tools <49209645+AgroClimaticTools@users.noreply.github.com>
2023-10-03 15:53:27 -0400

    Added KGE, Corrected dStat

--- dssatpylib/util_efficiency.py
+++ dssatpylib/util_efficiency.py
@@ -76,12 +76,46 @@ def R2(observed, simulated):
 
 def dStat(observed, simulated):
     '''
-    Performs the (one-sample or two-sample) Kolmogorov-Smirnov (d-stat) test for
-    goodness of fit between observed and simulated data
+    Performs Willmott (1981) Index of Agreement
+    Willmott (1981) proposed an index of agreement (d) as a standardized measure
+    of the degree of model prediction error which varies between 0 and 1. 
+    The index of agreement represents the ratio of the mean square error and 
+    the potential error. The agreement value of 1 indicates a perfect match, 
+    and 0 indicates no agreement at all. The index of agreement can detect 
+    additive and proportional differences in the observed and simulated means 
+    and variances; however, d is overly sensitive to extreme values due to the 
+    squared differences.
     
     :param  observed: numpy array or list of observed data
     :param simulated: numpy array or list of simulated data
     
     :return float: d-Stat between observed and simulated data
     '''
-    return stats.kstest(observed, simulated).pvalue
+    num = np.sum((observed-simulated)**2)
+    den = np.sum((abs(simulated-np.mean(observed)) + abs(observed-np.mean(observed)))**2)
+    dStat = 1-num/den
+    return dStat
+
+def KGE(observed, simulated):
+    '''
+    Performs Kling-Gupta efficiency
+    This goodness-of-fit measure was developed by Gupta et al. (2009) to provide
+    a diagnostically interesting decomposition of the Nash-Sutcliffe efficiency
+    (and hence MSE), which facilitates the analysis of the relative importance 
+    of its different components (correlation, bias and variability) in the
+    context of hydrological modelling Kling et al. (2012), proposed a revised
+    version of this index, to ensure that the bias and variability ratios are 
+    not cross-correlated.
+    
+    :param  observed: numpy array or list of observed data
+    :param simulated: numpy array or list of simulated data
+    
+    :return float: KGE between observed and simulated data
+    '''
+
+    correlation_coefficient, p_value = stats.pearsonr(observed, simulated)
+
+    KGE = 1 - ((correlation_coefficient - 1)**2 + \
+        ((np.std(observed)/np.std(simulated))-1)**2 + \
+        ((np.mean(observed)/np.mean(simulated))-1)**2)**0.5
+    return KGE
